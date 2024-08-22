387782ab832b6903a793edb8bc8b435befa3fa72
cfbj11 <cfbriones@uamv.edu.ni>
2024-08-21 21:02:03 -0600

    AreaCirculo

---
+++ EjerciciosPracticos/AreaCirculo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AreaCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio,area;
+            Console.WriteLine("Programa que calcula el area de un circulo.");
+            Console.Write("\nProporciona el radio del circulo: ");
+            radio = double.Parse(Console.ReadLine());
+            area = Math.PI * Math.Pow(radio,2);
+            Console.WriteLine($"\nEl area del circulo es: {area}");
+        }
+    }
+}
