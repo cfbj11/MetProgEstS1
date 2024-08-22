438286411bece83f0066ef08d6632ffbd8533b64
cfbj11 <cfbriones@uamv.edu.ni>
2024-08-21 21:08:28 -0600

    AumentarSalario

---
+++ EjerciciosPracticos/AumentarSalario/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AumentarSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, cargo;
+            double salario, aumento, nsalario;
+            Console.WriteLine("Programa que calcula el aumento de salario de un trabajador.");
+            Console.Write("\nNombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Cargo: ");
+            cargo = Console.ReadLine();
+            Console.Write("Salario: ");
+            salario = double.Parse(Console.ReadLine());
+            aumento = salario * 0.10;
+            nsalario = salario * 1.10;
+            Console.WriteLine($"\nNombre: {nombre} / Cargo: {cargo}");
+            Console.WriteLine($"Salario: {salario} / Aumento: {aumento} / Nuevo Salario: {nsalario}");
+        }
+    }
+}
