5c0f60a1b85af98885ade6fbe13fa31b4c1ea9e7
cfbj11 <cfbriones@uamv.edu.ni>
2024-08-21 20:53:20 -0600

    Saludar

---
+++ EjerciciosPracticos/Saludar/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Saludar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Ingresa tu nombre.");
+            Console.Write("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine($"\nHola {nombre}!");
+        }
+    }
+}
