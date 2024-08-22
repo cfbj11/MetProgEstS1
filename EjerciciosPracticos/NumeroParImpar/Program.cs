e6d6a506d53d83cd9ec1cb71621702528bb7f2ad
cfbj11 <cfbriones@uamv.edu.ni>
2024-08-21 21:14:03 -0600

    NumeroParImpar

---
+++ EjerciciosPracticos/NumeroParImpar/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NumeroParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Programa que determina si un numero es par o impar.");
+            Console.Write("\nIngrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"\n{num} es par.");
+            }
+            else
+            {
+                Console.WriteLine($"\n{num} es impar.");
+            }
+        }
+    }
+}
